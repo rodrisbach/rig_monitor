5c0e0c63b85607648aba1211d7141689ee658f48
Rodrigo Schanzenbach <schanzenbachsolo@gmail.com>
2021-12-05 22:06:51 -0300

    remove telegram python library

--- src/telegramBot.py
+++ src/telegramBot.py
@@ -1,8 +1,7 @@
-import json, logging, sys
+import json, logging
 from rig import Rig
 from apscheduler.schedulers.background import BackgroundScheduler
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from bootle import run,post
 
 def main():
 
@@ -26,39 +25,7 @@ def main():
     scheduler.start()
     rig = Rig(config["rig_url"],config["rig_password"],config["min_hashrate"],config["wallet_address"],config["coin"],config["flexpool_api"])
 #    scheduler.add_job(rig.verify_status,'interval', seconds=10, args=(updater))
-    updater.dispatcher.add_handler(CommandHandler('start',start))
-#    updater.dispatcher.add_handler(CommandHandler('status', status))
-    updater.dispatcher.add_handler(CommandHandler('status', lambda rig:status(rig)))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    # Start the bot
-    updater.start_polling()
-    # Run until the user stop the bot
-    updater.idle()
 
-
-def start(update: Update, context: CallbackContext):
-    keyboard = [
-        [
-            InlineKeyboardButton("status", callback_data="status"),
-            InlineKeyboardButton("restart", callback_data="restart"),
-            InlineKeyboardButton("shutdown", callback_data="shutdown"),
-            InlineKeyboardButton("config", callback_data="config"),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
-
-def status(update: Update, context: CallbackContext, rig):
-    message = rig.get_status()
-    update.message.reply_text(f'{message}')
-
-def button(update: Update, context: CallbackContext):
-    with open("config.json") as jsonfile:
-        config = json.load(jsonfile)
-    query = update.callback_query
-    query.answer()
-    if query.data == str("status"):
-        get_status(config["rig_url"], update)
 
 if __name__ == "__main__":
     main()
