de22480fdbcdbe697bb9f95b06b1251bbab8ebcb
Rodrigo Schanzenbach <schanzenbachsolo@gmail.com>
2021-12-26 17:40:08 -0300

    .

--- setup.sh
+++ setup.sh
@@ -14,3 +14,19 @@ cat <<EOF > api/config.json
     "telegram_username": "${TELEGRAM_USERNAME}",
 } 
 EOF
+
+cat <<EOF > scheduler/config.json
+{
+    "rig_url": "${RIG_URL}",
+    "rig_password": "${RIG_PASSWORD}",
+    "min_hashrate": "${MIN_HASHRATE}",
+    "wallet_address": "${WALLET_ADDRESS}",
+    "coin": "${COIN}",
+    "flexpool_api": "${FLEXPOOL_API}",
+    "log_path": "${LOG_PATH}",
+    "db_user": "${MYSQL_USER}",
+    "db_passwoord": "${MYSQL_PASSWORD}",
+    "db_host": "${}",
+    "db_database": "${MYSQL_DATABASE}"
+}
+EOF
